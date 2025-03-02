7d8c1dcba4514c51a0ff5247c3621b3485ad6f0f
Nazam-Kalsi <nzamkalsi@gmail.com>
2025-03-02 22:28:11 +0530

    initial commit-2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,14 @@ import {Box} from "./components"
 // import {useNavigate} from "react-router-dom"
 import { socket } from './socket';
 function App() {
-  const [data,setData]=useState("q");
+  const [data,setData]=useState("Not connected");
  
   socket.on('welcome', (arg) => {
     setData(arg);
   });
   return (
     <>
-     <h1>{data}</h1>
+     <h1 className='bg-red-600'>{data}</h1>
      <Box/>
     </>
   )
